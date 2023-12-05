215fc8d5f488fdea81330ec4e33a87ad8a4943ce
crypticsurfer <cb.connor.bishop@gmail.com>
2023-12-04 18:33:01 -0700

    gitignore stuff

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const routes = require("./controllers");
 const sequelize = require("./config/connection");
 const SequelizeStore = require("connect-session-sequelize")(session.Store);
 const app = express();
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3002;
 const hbs = exphbs.create();
 
 const sess = {
